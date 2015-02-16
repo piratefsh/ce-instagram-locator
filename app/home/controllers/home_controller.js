550748815ca881172dd4b2588426db31942fe579
Sher Minn Chong <schong7@wisc.edu>
2015-02-16 15:42:56 +0800

    Create angular base app

---
+++ app/home/controllers/home_controller.js
@@ -0,0 +1,3 @@
+angular.module('app.controllers').controller('HomeController', ['$scope', function($scope){
+  $scope.now = new Date();
+}]);
---
+++ assets/js/dist/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var controllers = angular.module('app.controllers', []);
+var factories = angular.module('app.factories', []);
+var models = angular.module('app.models', []);
+var directives = angular.module('app.directives', []);
+// var services = angular.module('app.services', []);
+var dashboard = angular.module('app', 
+  ['app.controllers', 'app.factories', 'app.models', 'app.directives',
+  'ngRoute']);
+
+angular.module('app').config(['$routeProvider', function($routeProvider){
+  $routeProvider
+    .when('/', {
+      templateUrl: 'app/home/views/home.html',
+      controller: 'HomeController'
+    })
+}]);
+angular.module('app.controllers').controller('HomeController', ['$scope', function($scope){
+  $scope.now = new Date();
+}]);
